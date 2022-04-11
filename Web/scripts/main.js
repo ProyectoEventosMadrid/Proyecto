d259a789c59f4634efa53e8afdc3da4c8e8291ad
Yaiza25 <yaizaetxaniz25@gmail.com>
2022-04-11 13:10:06 +0200

    Login arreglado. Filtro control y login control implementados

--- Web/scripts/main.js
+++ Web/scripts/main.js
@@ -1,15 +1,94 @@
-// Funciones de la barra de navegacion
-// Efecto de transparencia dependiendo de la zona donde se encuentre
+// Variables
 var bTransparent = true;
 
+// Proceso
 $(document).ready(function () {
-    // Cambio de color de la barra de navegación al desplazarse
+    // Cambiar transparencia de la barra de navegacion
     if ($('.navbar[color-on-scroll]').length != 0) {
-        $(window).on('scroll', pk.checkScrollForTransparentNavbar)
+        $(window).on('scroll', fNavbar.checkScrollForTransparentNavbar)
     }
+
+    // Obtener localStorage de sesion y si no existe, crearlo
+    if (localStorage.Session == null) {
+        var aSession = { "Sesion": false, "User": "", "Token": ""}; // Crear estructura del localStorage.Session
+        localStorage.Session = JSON.stringify(aSession); // Crear el localStorage.Session
+    } else {
+        var aSession = JSON.parse(localStorage.Session); // Obtener en Array el localStorage.Session
+    }
+
+    // Control del boton de busqueda. Si no hay nadie logeado, desactivarlo
+    if (!aSession["Sesion"]) { 
+        $('#btn-filter').prop("disabled", true);
+    } else {
+        $('#btn-filter').prop("disabled", false);
+    }
+
+    // Control boton login/salir. Si hay alguien logeado mostrar solo el boton salir, sino mostrar solo el boton login
+    if (aSession["Sesion"]) { 
+        $('#btn-login').hide();
+    } else {
+        $('#btn-salir').hide();
+    }
+
+    // Obtener el Token del admin
+    var sTokenAdmin = "";
+
+    $.ajax({
+        type: "POST",
+        dataType: "html",
+        async: false,
+        url: `http://localhost:5000/api/Users/authenticate/username/paulac/password/1234`,
+        headers: {
+            "accept": "application/json",
+        },
+        success: function (response) {
+            var aUsuario = JSON.parse(response);
+            sTokenAdmin = aUsuario.token;
+        }
+    });
+
+    // Obtener la lista de centros de los eventos
+    var aCentros = [];
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        async: false,
+        url: `http://localhost:5000/api/Eventos/Centros`,
+        headers: {
+            "accept": "application/json",
+            "Authorization": "Bearer " + sTokenAdmin
+        },
+        success: function (response) {
+            var aRespuestaCentros = JSON.parse(response);
+
+            aRespuestaCentros.forEach(centro => {
+                aCentros.push(centro.organizacion);
+            });
+        }
+    });
+
+    // Añadir calendario al input de fecha
+    $("#fecha-filter").datepicker({ dateFormat: 'dd/mm/yy' });
+    Inputmask().mask("fecha-filter");
+
+    // Añadir el autocomple al input de centros
+    $("#centros-filter").autocomplete({
+        source: aCentros
+    });
+
+    // Control click boton salir
+    $('#btn-salir').on('click', function (ev) {
+        var aSession = { "Sesion": false, "User": "", "Token": ""}; // Crear estructura del localStorage.Session
+        localStorage.Session = JSON.stringify(aSession); // Crear el localStorage.Session
+
+        window.location.href = "./index.html"; // Recargar pagina
+    })
 });
 
-var pk = {
+// Funciones de la barra de navegacion
+// Efecto de transparencia dependiendo de la zona donde se encuentre
+var fNavbar = {
     misc: {
         navbar_menu_visible: 0
     },
@@ -40,50 +119,3 @@ function debounce(func, wait, immediate) {
         if (immediate && !timeout) func.apply(context, args);
     };
 };
-
-// Funcion para el filtro de busqueda
-// Autocompleta el input busqueda y pone un calendario al input fecha
-$(document).ready(function () {
-    var sTokenAdmin = "";
-
-    $.ajax({ // Obtener el token del admin
-        type: "POST",
-        dataType: "html",
-        async: false,
-        url: `http://localhost:5000/api/Users/authenticate/username/pauca/password/1234`,
-        headers: {
-            "accept": "application/json",
-        },
-        success: function (response) {
-            var aUsuario = JSON.parse(response);
-            sTokenAdmin = aUsuario.token;
-        }
-    });
-
-    var aCentros = [];
-
-    $.ajax({ // Obtener la lista de centros de los eventos
-        type: "GET",
-        dataType: "html",
-        async: false,
-        url: `http://localhost:5000/api/Eventos/Centros`,
-        headers: {
-            "accept": "application/json",
-            "Authorization": "Bearer " + sTokenAdmin
-        },
-        success: function (response) {
-            var aRespuestaCentros = JSON.parse(response);
-
-            aRespuestaCentros.forEach(centro => {
-                aCentros.push(centro.organizacion);
-            });
-        }
-    });
-
-    $("#fecha-filter").datepicker({ dateFormat: 'dd/mm/yy' });
-    Inputmask().mask("fecha-filter");
-
-    $("#centros-filter").autocomplete({
-        source: aCentros
-    });
-});
